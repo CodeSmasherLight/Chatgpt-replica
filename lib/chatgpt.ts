0297b9deda2ca4a1cc4556c253d462b490a4a073
Satyam Singh <singhsatyam8383@gmail.com>
2024-09-27 04:06:44 +0530

    commit

--- lib/chatgpt.ts
+++ lib/chatgpt.ts
@@ -2,16 +2,6 @@ import Configuration from "openai";
 import OpenAIApi from "openai";
 
 
-// const configuration = new Configuration({
-//     organization:"org-UAPij8a0wQMcsyzsJG6ucMMP",
-//     apiKey: process.env.OPENAI_API_KEY,
-// });
-
-// const openai = new OpenAIApi(configuration);
-
-// export default openai;
-
-
 const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
 });
