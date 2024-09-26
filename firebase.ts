98434e0087d06cd6f5092a18a632040ce4ecca01
Satyam Singh <singhsatyam8383@gmail.com>
2024-09-27 03:59:13 +0530

    configured firebase

--- firebase.ts
+++ firebase.ts
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey:,
-  authDomain: "chatgpt-replica-64474.firebaseapp.com",
-  projectId: "chatgpt-replica-64474",
-  storageBucket: "chatgpt-replica-64474.appspot.com",
-  messagingSenderId: "1028087174125",
-  appId: "1:1028087174125:web:5e2441db776428b0decde4"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 };
 
 // Initialize Firebase
